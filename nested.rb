95dbd06b3d26dda7e282e98d6e6063d8b9d63300
Michael Espina <michael_espina@yahoo.com>
2019-10-23 00:44:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,9 +83,6 @@ def adding_matz
      }
 new = {:yukihiro_matsumoto => {:known_for => "Ruby",:languages => ["LISP", "C"]} }
 combined = new.merge(programmer_hash)
-programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby" 
-programmer_hash[:yukihiro_matsumoto][:languages][0] = "LISP"
-programmer_hash[:yukihiro_matsumoto][:languages][1] = "C"
 end
 
 def changing_alan
